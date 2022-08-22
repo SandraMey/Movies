a28c261ad4213f83b09b4c4379bcc031e77b4a98
SandraMey <sandra.meyniel@gmail.com>
2022-08-22 18:01:26 +0200

    add dotenv

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
   baseUrl: 'https://api.themoviedb.org/3/',
-  apiKey: '5039617074d7b10fd2990d6772523cf0',
+  apiKey: process.env.API_KEY,
   originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
   w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
